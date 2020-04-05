77fb60764e5d5e5fe088580a2bd1da5b72a7091d
Dai Kawahara <skbsporta7@gmail.com>
2020-04-05 16:28:38 +0900

    add CardViewController

---
+++ CardViewController-Sample/CardViewController/CardViewController.swift
@@ -0,0 +1,26 @@
+//
+//  CardViewController.swift
+//  CardViewController-Sample
+//
+//  Created by kawaharadai on 2020/04/05.
+//  Copyright © 2020 kawaharadai. All rights reserved.
+//
+
+import UIKit
+
+class CardViewController: UIViewController {
+
+    init() {
+        super.init(nibName: "CardViewController", bundle: nil)
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
